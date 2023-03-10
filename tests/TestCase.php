f1a770bc3c76a9289517dd6edc81531985b15efb
Brayan Martínez Santana <brayanmtz@ciencias.unam.mx>
2023-03-10 09:29:48 -0600

    tests: Cleanup test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,12 @@
 
 namespace BrayanCaro\LaravelApiRule\Tests;
 
-use BrayanCaro\LaravelApiRule\LaravelApiRuleServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'BrayanCaro\\LaravelApiRule\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            LaravelApiRuleServiceProvider::class,
-        ];
-    }
-
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-api-rule_table.php.stub';
-        $migration->up();
-        */
     }
 }
