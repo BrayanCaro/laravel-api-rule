cf6ce290cfe4b014b6715ed2fbbb057be25a449b
Brayan Martínez Santana <brayanmtz@ciencias.unam.mx>
2023-03-11 10:29:57 -0600

    test: Add tests for simple successful response

---
+++ tests/ApiRule/SuccessfulResponseTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use BrayanCaro\LaravelApiRule\ApiRule;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+
+beforeEach(function () {
+    Http::fake([
+        'dummy.com*' => Http::response([
+            'data' => [
+                'attribute1' => 100,
+                'attribute2' => false,
+                'obj' => [
+                    'id' => 12,
+                    'type' => 'TYPE1',
+                ],
+            ],
+        ]),
+    ]);
+    $this->rule = new class extends ApiRule
+    {
+        protected function pullResponse($value): Response
+        {
+            return Http::get('dummy.com');
+        }
+    };
+});
+
+it('passes missing data')
+    ->expect(fn ($value) => Validator::make(['foo' => $value], ['foo' => $this->rule]))
+    ->passes()
+    ->toBeTrue()
+    ->with([
+        '',
+        null,
+    ]);
+
+it('fails missing data, but required')
+    ->expect(fn ($value) => Validator::make(['foo' => $value], ['foo' => [$this->rule, 'required']]))
+    ->fails()
+    ->toBeTrue()
+    ->with([
+        '',
+        null,
+    ]);
+
+it('passes a successful request')
+    ->expect(fn () => Validator::make(['foo' => 'bar'], ['foo' => [$this->rule, 'required']]))
+    ->passes()
+    ->toBeTrue();
